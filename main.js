4eefe1acd6e1bc420df21237e4828b8a2bd870e1
Camilo318 <camiloa0318@gmail.com>
2020-04-06 18:18:46 -0500

    Cards were added. Now user can see info for the country he wants

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 
 let input = document.getElementById('userIn')
 
+let confirmed = document.getElementById('confirmed')
+let deaths = document.getElementById('deaths')
+let recovered = document.getElementById('recovered')
+
+let cards = document.querySelectorAll('.results__card')
+
+
 const API = 'https://api.covid19api.com/summary'
 
 input.addEventListener('keypress', (e) => checKey(e))
@@ -20,5 +27,15 @@ async function getData(userIn) {
     let countries = data.Countries
     let results = countries.filter(country => country.Slug === userIn)
     console.log(results)
+
+    confirmed.textContent = results[0].TotalConfirmed
+    deaths.textContent = results[0].TotalDeaths
+    recovered.textContent = results[0].TotalRecovered
+
+    cards[0].classList.add('show')
+    cards[1].classList.add('show')
+    cards[2].classList.add('show')
+
+
 
 }
